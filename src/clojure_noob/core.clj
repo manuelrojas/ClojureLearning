e0dd7583b009215ec95f382b4719073d665d1e6c
Manuel Rojas <manuel@seesawlabs.com>
2017-10-12 14:55:40 -0600

    destructure a vector

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -14,11 +14,26 @@
        "MILDLY INCONVENIENCED!"
        "DOOOOOOOMED!"))))
 
+(defn chooser
+  [[first-choice second-choice & unimportant-choices]]
+  (println (str "Your first choice is: " first-choice))
+  (println (str "Your second choice is: " second-choice))
+  (println (str "We're ignoring the rest of your choices. "
+    "Here they are in case you need to cry over them: "
+    (clojure.string/join ", " unimportant-choices))))
+  
+(defn announce-treasure-location
+  [{lat :lat lng :lng}]
+  (println (str "Treasure lat: " lat))
+  (println (str "Treasure lng: " lng)))
+
 (defn -main
   [& args]
   (error-message :mild)
   (too-enthusiastic :Manuel)
-  
+  (chooser ["Marmalade", "Handsome Jack", "Pigpen", "Aquaman"])
+  (announce-treasure-location {:lat 28.22 :lng 81.33})
+
   (if (= 1 2)
     (do (println "Success!")
       "By Zeus's hammer!")
