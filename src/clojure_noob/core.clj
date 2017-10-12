99bec9c81b0ff93a6fa792a8c183912106943b61
Manuel Rojas <manuel@seesawlabs.com>
2017-10-12 14:45:43 -0600

    Adding more code examples

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -1,7 +1,32 @@
 (ns clojure-noob.core
   (:gen-class))
 
+(defn too-enthusiastic
+  "Return a cheer that might be a bit too enthusiastic"
+  [name]
+  (println (str "OH. MY. GOD! " name " YOU ARE MOST DEFINITELY LIKE THE BEST "
+    "MAN SLASH WOMAN EVER I LOVE YOU AND WE SHOULD RUN AWAY SOMEWHERE")))
+
+(defn error-message
+  [severity]
+  (str "OH GOD! IT'S A DISASTER! WE'RE "
+  (if (= severity :mild)
+     "MILDLY INCONVENIENCED!"
+     "DOOOOOOOMED!")))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
+  (error-message :mild)
+  (too-enthusiastic :sup)
+  
+  (if (= 1 2)
+    (do (println "Success!")
+      "By Zeus's hammer!")
+    (do (println "Failure!")
+      "By Aquaman's trident!"))
+
+  (when true
+    (println "Success!")
+    "abra cadabra")
+
   (println "I'm a little teapot!"))
